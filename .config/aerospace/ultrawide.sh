a628ac7daeb0a09c3c5c481c0b6ae1045a322b18
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-20 07:33:15 +0300

    cmd centric bindings, manually toggled ultrawide mode

--- .config/aerospace/ultrawide.sh
+++ .config/aerospace/ultrawide.sh
@@ -4,18 +4,17 @@ WINDOW_COUNT=$(aerospace list-windows --workspace focused --count)
 MONITOR=$(aerospace list-monitors --focused --format "%{monitor-name}") 
 
 # Toggle layout for each window in focused workspace.
-toggle-workspace-layout() {
-  local layout=$1
+reset-workspace-to-tiling() {
   for window in $(aerospace list-windows --workspace focused --format "%{window-id}"); do
-    aerospace layout --window-id $window "$layout"
+    aerospace layout --window-id $window tiling
   done
 }
 
 # If using ultrawide display and there is only one window, 
 # make the single window float and set it to have sensible width
-if [[ "$WINDOW_COUNT" -eq 1 && "$MONITOR" == "T34w-30" ]]; then
-  toggle-workspace-layout floating
+if [[ "$WINDOW_COUNT" -eq 1 ]]; then
+  aerospace layout floating tiling
   open -g "rectangle://execute-action?name=center-half"
 else
-  toggle-workspace-layout tiling
+  reset-workspace-to-tiling
 fi
